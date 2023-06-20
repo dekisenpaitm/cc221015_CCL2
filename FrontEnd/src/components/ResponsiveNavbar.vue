e497d07fb5c50b2bb4ace00e744b4db17b5f3322
dekisenpaitm <116723190+dekisenpaitm@users.noreply.github.com>
2023-06-20 13:51:40 +0200

    To FIX:
    add some links to the footer
    add a logo
    
    currently running:
    localhost 8000
    david db
    
    TODO: documentation

--- FrontEnd/src/components/ResponsiveNavbar.vue
+++ FrontEnd/src/components/ResponsiveNavbar.vue
@@ -17,6 +17,10 @@
                         <li><router-link to="/register"><a v-if="!loggedIn">Register</a></router-link></li>
                 </ul>
             </div>
+            <div><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-terminal mx-2" viewBox="0 0 16 16">
+                <path d="M6 9a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3A.5.5 0 0 1 6 9zM3.854 4.146a.5.5 0 1 0-.708.708L4.793 6.5 3.146 8.146a.5.5 0 1 0 .708.708l2-2a.5.5 0 0 0 0-.708l-2-2z"/>
+                <path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h12z"/>
+            </svg></div>
             <router-link to="/"><a class="btn btn-ghost normal-case text-4xl">Portfolio</a></router-link>
         </div>
         <div class="navbar-center hidden lg:flex">
