23e816a9c1f72b670995b5e43de9cc88657e8c3d
dekisenpaitm <116723190+dekisenpaitm@users.noreply.github.com>
2023-06-22 16:45:42 +0200

    Final.final.final.final.final Version finished
    did readability as good as possible
    
    currently running:
    localhost 8000
    david db
    
    TODO: NOTHING

--- FrontEnd/src/components/ResponsiveNavbar.vue
+++ FrontEnd/src/components/ResponsiveNavbar.vue
@@ -3,7 +3,7 @@
         <div class="navbar-start">
             <div class="dropdown">
                 <label tabindex="0" class="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" alt="menubar"/></svg>
                 </label>
                 <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                     <router-link to="/games"><li><a>GameDev</a></li></router-link>
